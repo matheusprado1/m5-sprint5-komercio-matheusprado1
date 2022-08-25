50e78f9fe28b16409007fea684ea2fcb6b40982f
matheusprado1 <matheusprado1096@gmail.com>
2022-08-25 13:14:27 -0300

    feat: creating permission for seller

--- products/permissions.py
+++ products/permissions.py
@@ -9,3 +9,11 @@ class IsProductOwner(permissions.BasePermission):
             request.user.is_authenticated
             and request.user.is_seller
         )
+
+
+class IsSeller(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user.is_authenticated and request.user.is_seller
