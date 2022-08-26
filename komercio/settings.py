2a8fd535fa7424779fa4eaa2f7d4af7746575e3a
matheusprado1 <matheusprado1096@gmail.com>
2022-08-26 10:27:12 -0300

    feat: comment dotenv

--- komercio/settings.py
+++ komercio/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 '''
 import os
-import dotenv
+
+# import dotenv
 
 import dj_database_url
 from pathlib import Path
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-dotenv.load_dotenv()
+# dotenv.load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
